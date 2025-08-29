886dc8db833faf0bfcdc56b7e714579a537654dd
hotateishi2020 <hotateishi2018@gmail.com>
2025-08-29 11:14:42 +0900

    Web公開のための修正6

--- backend/public/flutter_bootstrap.js
+++ backend/public/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2507097812"
+    serviceWorkerVersion: "1292909597"
   }
 });

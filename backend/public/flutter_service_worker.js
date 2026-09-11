bee09a423e365c4524e17cc0844a062041d7c5dc
hotateishi2020 <hotateishi2018@gmail.com>
2026-09-11 16:31:25 +0900

    フロントの Web ビルドを更新

--- backend/public/flutter_service_worker.js
+++ backend/public/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "371755755a99c39eec5bb490adbef899",
+const RESOURCES = {"flutter_bootstrap.js": "1b59a1096e0aa0bb7035f59edb10cc12",
 "version.json": "90f0a4de3d79c28df80f8b271963e5d5",
 "index.html": "e85c3a2376f9bfeba930213e57566491",
 "/": "e85c3a2376f9bfeba930213e57566491",
-"main.dart.js": "e9c616ce03410570eec00f3fed27da8b",
+"main.dart.js": "650eb13b5fc841d59a0f32837bfb639c",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
